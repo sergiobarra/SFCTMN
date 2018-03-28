915877276563861bfb87927e72eef97438d1f4e2
Francisco Wilhelmi <francisco.wilhelmi@upf.edu>
2018-03-28 10:40:59 +0200

    Code adapted to Seminar 4 (WMNs)

--- source/sfctm/system_conf.m
+++ source/sfctm/system_conf.m
@@ -10,14 +10,14 @@
 
 % Throughput
 PACKET_ERR_PROBABILITY = 0.0;                   % Packet error probability
-NUM_PACKETS_AGGREGATED = 1;                    % Number of aggregated packets in each transmission
+NUM_PACKETS_AGGREGATED = 1;                     % Number of aggregated packets in each transmission
 PACKET_LENGTH = 12000;                          % Packet length [bits]
 SINGLE_USER_SPATIAL_STREAMS = 1;                % Single User Spatial Streams
 MCS_INDEX = 11;                                 % Modulation Coding Scheme index
 
 % Power
 CCA_DEFAULT = -82;                              % CCA level [dBm]
-CAPTURE_EFFECT = -10000;                            % Capture effect [dB]
+CAPTURE_EFFECT = 10;                            % Capture effect [dB]
 POWER_TX_DEFAULT = 20;                          % Transmission power [dBm]
 GAIN_TX_DEFAULT = 0;                            % Transmitter gain [dB]
 GAIN_RX_DEFAULT = 0;                            % Receiver gain [dB]
@@ -27,14 +27,10 @@ path_loss_model = PATH_LOSS_WMN_SEMINAR;            % Path loss model index
 access_protocol_type = ACCESS_PROTOCOL_IEEE80211;   % Access protocol type
 flag_hardcode_distances = false;                    % Allows hardcoding distances from main_sfctmn.m file
 carrier_frequency = 5E9;                            % Carrier frequency [MHz] (2.4 or 5) GHz
-NOISE_DBM = -1000;                                    % Ambient noise [dBm]
+NOISE_DBM = -95;                                    % Ambient noise [dBm]
 
 % DSA policy type
 dsa_policy_type = DSA_POLICY_ONLY_MAX;          % Always-max
-% dsa_policy_type = DSA_POLICY_ONLY_MAX;              % SCB
-% dsa_policy_type = DSA_POLICY_ONLY_PRIMARY;          % Only-primary
-% dsa_policy_type = DSA_POLICY_EXPLORER_UNIFORM;    % Probabilistic uniform
-% dsa_policy_type = DSA_POLICY_EXPLORER_LADDER;
 
 save('system_conf.mat');  % Save system configuration into current folder
 disp('System configuration saved in file system_conf.mat')
